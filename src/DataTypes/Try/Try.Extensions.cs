369997d8da7bb49751a7eea6e903fd9bbfb8a618
francomelandri <franco.melandri@gmail.com>
2021-04-26 16:00:11 +0200

    improving Try type-class

--- src/DataTypes/Try/Try.Extensions.cs
+++ src/DataTypes/Try/Try.Extensions.cs
@@ -1,50 +1,110 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
-using TinyFp;
 using TinyFp.Common;
 
-public static class TryExtensions
+namespace TinyFp
 {
-    public static Try<A> Memo<A>(this Try<A> ma)
+    public static class TryExtensions
     {
-        bool run = false;
-        var result = new Result<A>();
-        return () =>
+        public static Try<A> Memo<A>(this Try<A> ma)
         {
-            if (run) return result;
-            var ra = ma.Try();
-            if (result.IsSuccess)
+            var run = false;
+            var result = new Result<A>();
+            return () =>
             {
-                run = true;
-                result = ra;
+                if (run) return result;
+                var ra = ma.Try();
+                if (result.IsSuccess)
+                {
+                    run = true;
+                    result = ra;
+                }
+                return ra;
+            };
+        }
+
+        [Pure]
+        public static Result<T> Try<T>(this Try<T> self)
+        {
+            try
+            {
+                if (self == null)
+                {
+                    throw new ArgumentNullException(nameof(self));
+                }
+                return self();
             }
-            return ra;
+            catch (Exception e)
+            {
+                return new Result<T>(e);
+            }
+        }
+
+        [Pure]
+        public static R Match<A, R>(this Try<A> self, Func<A, R> Succ, Func<Exception, R> Fail)
+        {
+            var res = self.Try();
+            return res.IsSuccess ?
+                Succ(res.Value) :
+                Fail(res.Exception);
+        }
+
+        [Pure]
+        public static R Match<A, R>(this Try<A> self, Func<A, R> Succ, R Fail)
+        {
+            var res = self.Try();
+            return res.IsFaulted ?
+                Fail :
+                Succ(res.Value);
+        }
+
+        [Pure]
+        public static A OnFail<A>(this Try<A> self, A failValue)
+            => OnFail(self, () => failValue);
+
+        [Pure]
+        public static A OnFail<A>(this Try<A> self, Func<A> Fail)
+        {
+            var res = self.Try();
+            return res.IsSuccess ?
+                res.Value :
+                Fail();
+        }
+
+        [Pure]
+        public static Either<Exception, A> ToEither<A>(this Try<A> self)
+        {
+            var res = self.Try();
+            return res.IsFaulted ?
+                Either<Exception, A>.Left(res.Exception) :
+                Either<Exception, A>.Right(res.Value);
+        }
+
+        [Pure]
+        public static Try<B> Bind<A, B>(this Try<A> ma, Func<A, Try<B>> f)
+            => Memo(() =>
+                {
+                    try
+                    {
+                        var ra = ma.Try();
+                        return ra.IsSuccess ?
+                            f(ra.Value)() :
+                            new Result<B>(ra.Exception);
+                    }
+                    catch (Exception e)
+                    {
+                        return new Result<B>(e);
+                    }
+                });
+
+        public static Try<A> Do<A>(this Try<A> ma, Action<A> f) => () =>
+        {
+            var r = ma.Try();
+            if (!r.IsFaulted)
+            {
+                f(r.Value);
+            }
+            return r;
         };
-    }
-
-    [Pure]
-    public static Result<T> Try<T>(this Try<T> self)
-    {
-        try
-        {
-            if (self == null)
-            {
-                throw new ArgumentNullException(nameof(self));
-            }
-            return self();
-        }
-        catch (Exception e)
-        {
-            return new Result<T>(e);
-        }
-    }
-
-    [Pure]
-    public static R Match<A, R>(this Try<A> self, Func<A, R> Succ, Func<Exception, R> Fail)
-    {
-        var res = self.Try();
-        return res.IsSuccess ? 
-            Succ(res.Value) : 
-            Fail(res.Exception);
     }
 }
