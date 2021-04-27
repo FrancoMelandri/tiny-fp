c7d5ebe3573d98accceef2c181289afc939c3e19
francomelandri <franco.melandri@gmail.com>
2021-04-27 15:59:22 +0200

    improving TryAsync features

--- src/DataTypes/TryAsync/TryAsync.Extensions.cs
+++ src/DataTypes/TryAsync/TryAsync.Extensions.cs
@@ -24,6 +24,23 @@ namespace TinyFp
             }
         }
 
+        public static TryAsync<A> Memo<A>(this TryAsync<A> ma)
+        {
+            bool run = false;
+            var result = Result<A>.Bottom;
+            return new TryAsync<A>(async () =>
+            {
+                if (run) return result;
+                var tra = ma.Try();
+                var ra = await tra;
+                if (ra.IsSuccess)
+                {
+                    result = ra;
+                    run = true;
+                }
+                return ra;
+            });
+        }
         [Pure]
         public static async Task<R> Match<A, R>(this TryAsync<A> self, Func<A, R> Succ, Func<Exception, R> Fail)
         {
@@ -77,5 +94,97 @@ namespace TinyFp
                 ? Fail
                 : await Succ(res.Value);
         }
+
+        [Pure]
+        public static async Task<A> OnFail<A>(this TryAsync<A> self, A failValue)
+        {
+            try
+            {
+                var res = await self.Try();
+                return res.IsSuccess ?
+                    res.Value :
+                    failValue;
+            }
+            catch
+            {
+                return failValue;
+            }
+        }
+
+        [Pure]
+        public static async Task<A> OnFail<A>(this TryAsync<A> self, Func<Task<A>> Fail)
+            => await self.OnFail(await Fail());
+
+        [Pure]
+        public static async Task<A> OnFail<A>(this TryAsync<A> self, Func<A> Fail)
+            => await self.OnFail(Fail());
+
+        [Pure]
+        public static async Task<A> OnFail<A>(this TryAsync<A> self, Func<Exception, A> Fail)
+        {
+            try
+            {
+                var res = await self.Try();
+                return res.IsSuccess ?
+                    res.Value :
+                    Fail(res.Exception);
+            }
+            catch (Exception e)
+            {
+                return Fail(e);
+            }
+        }
+
+        [Pure]
+        public static async Task<A> OnFail<A>(this TryAsync<A> self, Func<Exception, Task<A>> Fail)
+        {
+            try
+            {
+                var res = await self.Try();
+                return res.IsSuccess ?
+                    res.Value :
+                    await Fail(res.Exception);
+            }
+            catch (Exception e)
+            {
+                return await Fail(e);
+            }
+        }
+
+        [Pure]
+        public static async Task<Either<Exception, A>> ToEither<A>(this TryAsync<A> self)
+        {
+            var res = await self.Try();
+            return res.IsFaulted ?
+                Either<Exception, A>.Left(res.Exception) :
+                Either<Exception, A>.Right(res.Value);
+        }
+
+        [Pure]
+        public static TryAsync<B> Bind<A, B>(this TryAsync<A> ma, Func<A, TryAsync<B>> f) 
+            => Memo(async () =>
+                {
+                    try
+                    {
+                        var ra = await ma();
+                        return ra.IsSuccess ?
+                            await f(ra.Value)() :
+                            new Result<B>(ra.Exception);                        
+                    }
+                    catch (Exception e)
+                    {
+                        return new Result<B>(e);
+                    }
+                });
+
+        public static TryAsync<A> Do<A>(this TryAsync<A> ma, Action<A> f) => new TryAsync<A>(async () =>
+        {
+            var r = await ma.Try();
+            if (!r.IsFaulted)
+            {
+                f(r.Value);
+            }
+            return r;
+        });
     }
 }
--- test/DataTypes/TryAsync/TryAsyncTests.cs
+++ test/DataTypes/TryAsync/TryAsyncTests.cs
@@ -97,5 +97,123 @@ namespace TinyFpTest
                        Unit.Default)
             .GetAwaiter().GetResult()
             .Should().Be(Unit.Default);
+
+        [Test]
+        public void TryAsyncOnFail_WhenNoException_Value()
+            => TryAsync(() => GetValueAsync(10))                
+                .OnFail(0)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_WhenException_Fallback()
+            => TryAsync(() => GetValueAsync(0))                
+                .OnFail(10)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_FuncTask_WhenNoException_Value()
+            => TryAsync(() => GetValueAsync(10))
+                .OnFail(() => Task.FromResult(0))
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_FuncTask_WhenException_Fallback()
+            => TryAsync(() => GetValueAsync(0))
+                .OnFail(() => Task.FromResult(10))
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_Func_WhenNoException_Value()
+            => TryAsync(() => GetValueAsync(10))
+                .OnFail(() => 0)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_Func_WhenException_Fallback()
+            => TryAsync(() => GetValueAsync(0))
+                .OnFail(() => 10)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_FuncEx_WhenNoException_Value()
+            => TryAsync(() => GetValueAsync(10))
+                .OnFail(ex => 0)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_FuncEx_WhenException_Fallback()
+            => TryAsync(() => GetValueAsync(0))
+                .OnFail(ex => ex.Message == "Attempted to divide by zero." ? 10 : 0)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_FuncExTask_WhenNoException_Value()
+            => TryAsync(() => GetValueAsync(10))
+                .OnFail(ex => Task.FromResult(0))
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncOnFail_FuncExTask_WhenException_Fallback()
+            => TryAsync(() => GetValueAsync(0))
+                .OnFail(ex => Task.FromResult(ex.Message == "Attempted to divide by zero." ? 10 : 0))
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryAsyncToEither_WhenNoException_Right()
+            => TryAsync(() => GetValueAsync(10))
+                .ToEither()
+                .Result
+                .IsRight
+                .Should().BeTrue();
+
+        [Test]
+        public void TryAsyncToEither_WhenException_Left()
+            => TryAsync(() => GetValueAsync(0))
+                .ToEither()
+                .Result
+                .IsLeft
+                .Should().BeTrue();
+
+        [Test]
+        public void TryAsyncBind_WhenNoException_Call()
+            => TryAsync(() => GetValueAsync(10))
+                .Bind(_ => TryAsync(() => GetValueAsync(5)))                
+                .OnFail(0)
+                .Result
+                .Should().Be(20);
+
+        [Test]
+        public void TryBind_WhenFirstException_DontCall()
+            => TryAsync(() => GetValueAsync(0))
+                .Bind(_ => { Assert.Fail(); return TryAsync(() => GetValueAsync(5)); })
+                .OnFail(0)
+                .Result
+                .Should().Be(0);
+
+        [Test]
+        public void TryDo_WhenNoException_Call()
+            => TryAsync(() => GetValueAsync(10))
+                .Do(_ => { })
+                .OnFail(0)
+                .Result
+                .Should().Be(10);
+
+        [Test]
+        public void TryDo_WhenException_DontCall()
+            => TryAsync(() => GetValueAsync(0))
+                .Do(_ => { })
+                .OnFail(0)
+                .Result
+                .Should().Be(0);
     }
 }
