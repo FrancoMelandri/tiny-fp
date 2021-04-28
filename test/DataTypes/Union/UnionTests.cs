8abcd4f552e8ba18035f5ffeabfced7970eb1461
francomelandri <franco.melandri@gmail.com>
2021-04-28 15:27:30 +0200

    improving Union tests

--- test/DataTypes/Union/UnionTests.cs
+++ test/DataTypes/Union/UnionTests.cs
@@ -2,6 +2,7 @@
 using TinyFp;
 using FluentAssertions;
 using NUnit.Framework;
+using System;
 
 namespace TinyFpTest.DataTypes
 {
@@ -18,12 +19,32 @@ namespace TinyFpTest.DataTypes
         }
 
         [Test]
+        public void FromT1_WhenNull_RaiseException()
+        {
+            Action act = () => Union<object, object>.FromT1(null);
+
+            act.Should()
+                .Throw<ArgumentNullException>()
+                .And.ParamName.Should().Be("t1");
+        }
+
+        [Test]
         public void FromT2_CreateT1Object()
         {
             var t2 = Union<int, string>.FromT2("t2");
 
             t2.IsT1.Should().BeFalse();
             t2.IsT2.Should().BeTrue();
+        }
+
+        [Test]
+        public void FromT2_WhenNull_RaiseException()
+        {
+            Action act = () => Union<object, object>.FromT2(null);
+
+            act.Should()
+                .Throw<ArgumentNullException>()
+                .And.ParamName.Should().Be("t2");
         }
 
         [Test]
@@ -36,7 +57,7 @@ namespace TinyFpTest.DataTypes
         public void Match_WhenT2_ToOutput()
             => Union<int, string>.FromT2("union")
                     .Match(_ => false, _ => true)
-                    .Should().BeTrue();                
+                    .Should().BeTrue();
 
         [Test]
         public void MatchAsync_WhenT1_ToOutput()
@@ -50,6 +71,24 @@ namespace TinyFpTest.DataTypes
             => Union<int, string>.FromT2("union")
                     .MatchAsync(_ => Task.FromResult(false), _ => Task.FromResult(true))
                     .Result
-                    .Should().BeTrue();                
+                    .Should().BeTrue();
+
+        [Test]
+        public void WhenT1_ImplicitCast()
+        {
+            Union<int, string> result = 1;
+
+            result.IsT1.Should().BeTrue();
+            result.IsT2.Should().BeFalse();
+        }
+
+        [Test]
+        public void WhenT2_ImplicitCast()
+        {
+            Union<int, string> result = "union";
+
+            result.IsT2.Should().BeTrue();
+            result.IsT1.Should().BeFalse();
+        }
     }
 }
