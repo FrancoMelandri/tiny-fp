f08d7b2f7ebf5e390dbe70a19591d7dd80e2e10d
Jacopo <jacopo.riciputi@gmail.com>
2023-12-12 22:07:20 +0100

    Implemented method Filter for Optional data type

---
+++ src/DataTypes/Option/Option.Filter.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace TinyFp
+{
+    public partial struct Option<A>
+    {
+
+        [Pure]
+        public Option<A> Filter(Predicate<A> predicate)
+            => predicate == null ? this :
+                                    IsNone ? this : 
+                                            predicate(_value) ? this : None();
+                                                
+    }
+}
