54c6495ef83827261ea729e27c815864972f113a
Franco Melandri <franco.melandri@gmail.com>
2024-10-14 10:28:35 +0200

    intrducing functional While() to warp the while keyboard in a funcitonal way

---
+++ src/Extensions/Functional.Loops.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TinyFp.Extensions;
+
+public static partial class Functional
+{
+    public static T While<T>(Func<T> init, Func<T, bool> when, Func<T, T> execute)
+    {
+        var acc = init();
+        while (when(acc))
+            acc = execute(acc);
+        return acc;
+    }
+
+    public async static Task<T> While<T>(Func<T> init, Func<T, bool> when, Func<T, Task<T>> execute)
+    {
+        var acc = init();
+        while (when(acc))
+            acc = await execute(acc);
+        return acc;
+    }
+
+    public static async Task<T> While<T>(Func<Task<T>> init, Func<T, bool> when, Func<T, Task<T>> execute)
+    {
+        var acc = await init();
+        return await While(() => acc, when, execute);
+    }
+}
---
+++ test/Extensions/LoopExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Moq;
+using static TinyFp.Extensions.Functional;
+using TinyFp;
+
+namespace TinyFpTest.Extensions;
+
+[TestFixture]
+public class LoopExtensionsTests
+{
+    [Test]
+    public void While_Should_Aggregate()
+    {
+        var counter = While(() => 0,
+            _ => _ < 10,
+            _ => ++_);
+
+        counter.Should().Be(10);
+    }
+
+    [Test]
+    public void WhileAsync_Should_Aggregate()
+    {
+        var counter = While(() => 0,
+                _ => _ < 10,
+                _ => (++_).AsTask())
+            .Result;
+
+        counter.Should().Be(10);
+    }
+
+    [Test]
+    public void WhileAsync_Should_Aggregate_WhenAsyncInit()
+    {
+        var counter = While(() => 0.AsTask(),
+                _ => _ < 10,
+                _ => (++_).AsTask())
+            .Result;
+
+        counter.Should().Be(10);
+    }
+}
