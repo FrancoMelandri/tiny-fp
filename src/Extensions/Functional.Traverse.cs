92f204d8f6433abbd8289fbd873afa3f5c69b2ff
Franco Melandri <franco.melandri@gmail.com>
2024-10-14 10:37:32 +0200

    adding either array Traverse() extension

---
+++ src/Extensions/Functional.Traverse.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Contracts;
+using static TinyFp.Prelude;
+
+namespace TinyFp.Extensions;
+
+public static partial class Functional
+{
+    [Pure]
+    public static Either<L, R[]> Traverse<L, T, R>(
+        this Either<L, T>[] values,
+        Func<T, R> onRight)
+        => values
+        .Fold(Right<L, R[]>([]),
+             (acc, either) => acc.Bind(_ => either.Map(__ => _.Append(onRight(__)).ToArray())));
+
+    [Pure]
+    public static Either<L, T[]> Traverse<L, T>(
+        this Either<L, T>[] values)
+        => values.Traverse(_ => _);
+
+    [Pure]
+    public static Task<Either<L, R>[]> Traverse<L, T, R>
+        (this Func<T, Task<Either<L, R>>>[] funcs,
+        Task<Either<L, T>> @value)
+        => funcs.Fold(new List<Task<Either<L, R>>>(),
+                     (state, current) => state.Concat([@value.BindAsync(_ => current(_))]).ToList())
+        .Map(Task.WhenAll)
+        .MapAsync(_ => _.ToArray().AsTask());
+}
---
+++ test/Extensions/TraverseExtensionsTest.cs
@@ -0,0 +1,110 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TinyFp;
+using TinyFp.Extensions;
+
+namespace TinyFpTest.Extensions;
+
+internal class TraverseExtensionsTest
+{
+    [Test]
+    public void Traverse_Either_WithTransformation_ShouldReturnTransformedArray()
+    {
+        var values = new[]
+        {
+            Either<string, int>.Right(1),
+            Either<string, int>.Right(2),
+            Either<string, int>.Right(3)
+        };
+
+        var result = values.Traverse(x => x.ToString());
+
+        result.IsRight.Should().BeTrue();
+        result.Unwrap().Should().BeEquivalentTo(["1", "2", "3"]);
+    }
+
+    [Test]
+    public void Traverse_Either_WithLeftValue_ShouldReturnLeft()
+    {
+        var values = new[]
+        {
+            Either<string, int>.Right(1),
+            Either<string, int>.Left("Error"),
+            Either<string, int>.Right(3)
+        };
+
+        var result = values.Traverse(x => x.ToString());
+
+        result.IsLeft.Should().BeTrue();
+        result.UnwrapLeft().Should().Be("Error");
+    }
+
+    [Test]
+    public void Traverse_Either_WithoutTransformation_ShouldReturnSameArray()
+    {
+        var values = new[]
+        {
+            Either<string, int>.Right(1),
+            Either<string, int>.Right(2),
+            Either<string, int>.Right(3)
+        };
+
+        var result = values.Traverse();
+
+        result.IsRight.Should().BeTrue();
+        result.Unwrap().Should().BeEquivalentTo(new[] { 1, 2, 3 });
+    }
+
+    [Test]
+    public void Traverse_Either_WithoutTransformation_WithLeftValue_ShouldReturnLeft()
+    {
+        var values = new[]
+        {
+            Either<string, int>.Right(1),
+            Either<string, int>.Left("Error"),
+            Either<string, int>.Right(3)
+        };
+
+        var result = values.Traverse();
+
+        result.IsLeft.Should().BeTrue();
+        result.UnwrapLeft().Should().Be("Error");
+    }
+
+    [Test]
+    public async Task Traverse_AsyncFuncs_ShouldReturnArrayOfEithers()
+    {
+        Func<int, Task<Either<string, string>>>[] funcs = {
+            async x => await Task.FromResult(Either<string, string>.Right($"Value {x}")),
+            async x => await Task.FromResult(Either<string, string>.Right($"Value {x + 1}")),
+            async x => await Task.FromResult(Either<string, string>.Right($"Value {x + 2}"))
+        };
+
+        var value = Task.FromResult(Either<string, int>.Right(1));
+
+        var result = await funcs.Traverse(value);
+
+        result.Should().BeEquivalentTo(new[] {
+            Either<string, string>.Right("Value 1"),
+            Either<string, string>.Right("Value 2"),
+            Either<string, string>.Right("Value 3")
+        });
+    }
+
+    [Test]
+    public async Task Traverse_AsyncFuncs_WithLeftValue_ShouldReturnArrayWithLeft()
+    {
+        Func<int, Task<Either<string, string>>>[] funcs = {
+            async x => await Task.FromResult(Either<string, string>.Right($"Value {x}")),
+            async x => await Task.FromResult(Either<string, string>.Left("Error")),
+            async x => await Task.FromResult(Either<string, string>.Right($"Value {x + 2}"))
+        };
+
+        var value = Task.FromResult(Either<string, int>.Right(1));
+
+        var result = await funcs.Traverse(value);
+
+        result[1].IsLeft.Should().BeTrue();
+        result[1].UnwrapLeft().Should().Be("Error");
+    }
+}
